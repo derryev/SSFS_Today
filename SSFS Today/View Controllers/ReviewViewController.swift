1ba4128ee97d51aa51c303df4cb74e35869158f0
derryev <derryev@ssfs.org>
2024-05-16 11:56:50 -0400

    created new page that opens menu link

---
+++ SSFS Today/View Controllers/ReviewViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ReviewViewController.swift
+//  SSFS Today
+//
+//  Created by Eva Derryberry on 5/16/24.
+//  Copyright © 2024 Brian Wilkinson. All rights reserved.
+//
+
+import Foundation
+
+
+
+import UIKit
+import WebKit
+
+class ReviewViewController: UIViewController, WKNavigationDelegate {
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
